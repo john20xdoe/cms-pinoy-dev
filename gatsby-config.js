b4a224cb484bdb66833ef5a63201e90189b44797
Lee Alexis <john20xdoe@users.noreply.github.com>
2019-11-24 12:48:46 +0800

    Rename areas to Pinoy.dev

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,11 +1,11 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Starter Blog`,
-    author: `Kyle Mathews`,
-    description: `A starter blog demonstrating what Gatsby can do.`,
+    title: `Pinoy\.Dev`,
+    author: `pinoy.dev`,
+    description: `A site for Pinoy Devs`,
     siteUrl: `https://gatsby-starter-blog-demo.netlify.com/`,
     social: {
-      twitter: `kylemathews`,
+      twitter: ``,
     },
   },
   plugins: [
